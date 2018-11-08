bf414d5abdeac49509174bb330c698730258fba1
kevin-zz <kevin.z.zhang@outlook.com>
2018-11-07 22:24:30 -0500

    add styling to login

--- login.js
+++ login.js
@@ -1,41 +1,46 @@
+const showPasswordCheckbox = document.querySelector("#show-password");
+
+showPasswordCheckbox.addEventListener("click", revealPwd);
+
+
 function revealPwd() {
-    var x = document.getElementById("newPwd");
-    var y = document.getElementById("repeatPwd");
-    if (x.type === "password" && y.type === "password") {
-        x.type = "text";
-        y.type = "text";
-    } else {
-        x.type = "password";
-        y.type = "password";
-    }
+  var x = document.getElementById("newPwd");
+  var y = document.getElementById("repeatPwd");
+  if (x.type === "password" && y.type === "password") {
+    x.type = "text";
+    y.type = "text";
+  } else {
+    x.type = "password";
+    y.type = "password";
+  }
 }
 
 const userList = {}
 
 class User {
-    constructor(id, pwd, email, name) {
-        this.id = id;
-        this.pwd = pwd;
-        this.email = email;
-        this.name = name;
-        this.role = [];
-    }
+  constructor(id, pwd, email, name) {
+    this.id = id;
+    this.pwd = pwd;
+    this.email = email;
+    this.name = name;
+    this.role = [];
+  }
 
-    changeName(newName) {
-      this.name = newName;
-    }
+  changeName(newName) {
+    this.name = newName;
+  }
 
-    changePwd(newPwd) {
-        this.pwd = newPwd;
-    }
+  changePwd(newPwd) {
+    this.pwd = newPwd;
+  }
 
-    changeEmail(newEmail) {
-        this.email = newEmail;
-    }
+  changeEmail(newEmail) {
+    this.email = newEmail;
+  }
 
-    addRole(role) {
-        this.role.push(role);
-    }
+  addRole(role) {
+    this.role.push(role);
+  }
 }
 
 // Fake data
@@ -51,62 +56,62 @@ const registerForm = document.querySelector("#registerForm")
 const loginForm = document.querySelector("#loginForm")
 
 if (registerForm) {
-    registerForm.addEventListener('submit', addNewUser)
+  registerForm.addEventListener('submit', addNewUser)
 }
 if (loginForm) {
-    loginForm.addEventListener('submit', logIn)
+  loginForm.addEventListener('submit', logIn)
 }
 
 
 function addNewUser(e) {
-    e.preventDefault()
+  e.preventDefault()
 
-    var id = registerForm.newId.value;
-    var pwd = registerForm.newPwd.value;
-    var pwd2 = registerForm.repeatPwd.value;
-    var email = registerForm.newEmail.value;
-    var name = registerForm.legalName.value;
+  var id = registerForm.newId.value;
+  var pwd = registerForm.newPwd.value;
+  var pwd2 = registerForm.repeatPwd.value;
+  var email = registerForm.newEmail.value;
+  var name = registerForm.legalName.value;
 
-    if (id in userList) {
-        if (email === userList[id].email) {
-            //Replace with pop up or some notification on webpage
-            console.log("existing profile, try remembering your password")
-        } else {
-            //Replace with pop up or some notification on webpage
-            console.log("existing username, choose different user ID")
-        }
-        return
+  if (id in userList) {
+    if (email === userList[id].email) {
+      //Replace with pop up or some notification on webpage
+      console.log("existing profile, try remembering your password")
+    } else {
+      //Replace with pop up or some notification on webpage
+      console.log("existing username, choose different user ID")
     }
+    return
+  }
 
-    if (pwd !== pwd2) {
-        console.log("password not matching")
-        return // pwd not matching
-    }
+  if (pwd !== pwd2) {
+    console.log("password not matching")
+    return // pwd not matching
+  }
 
-    userList[id] = new User(id, pwd, email, name)
-    console.log(userList[id])
+  userList[id] = new User(id, pwd, email, name)
+  console.log(userList[id])
 }
 
 function logIn(e) {
-    e.preventDefault()
+  e.preventDefault()
 
-    var userid = loginForm.userid.value;
-    var password = loginForm.pwd.value;
+  var userid = loginForm.userid.value;
+  var password = loginForm.pwd.value;
 
-    if (userid in userList) {
-        if (userList[userid].pwd === password) {
-            if ("admin" in userList[userid].role) {
-                window.location.href = "main-admin.html"
-            } else if ("landlord" in userList[userid].role) {
-                window.location.href = "landlord-view.html"
-            } else if ("tenant" in userList[userid].role) {
-                window.location.href = "main-tenant.html"
-            }
-            return //login
-        }
-        console.log("Wrong Password")
-        return //fail
+  if (userid in userList) {
+    if (userList[userid].pwd === password) {
+      if ("admin" in userList[userid].role) {
+        window.location.href = "main-admin.html"
+      } else if ("landlord" in userList[userid].role) {
+        window.location.href = "landlord-view.html"
+      } else if ("tenant" in userList[userid].role) {
+        window.location.href = "main-tenant.html"
+      }
+      return //login
     }
-    console.log("user does not exist")
+    console.log("Wrong Password")
     return //fail
+  }
+  console.log("user does not exist")
+  return //fail
 }
