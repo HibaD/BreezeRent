5723bd543e2d964d086a036dfc149e30a0e5aa89
kevin-zz <kevin.z.zhang@outlook.com>
2018-12-04 16:10:39 -0500

    add statis paths to enable css and scripts

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
+app.use('/Styles',  express.static(__dirname + '/Styles'));
+app.use('/Scripts', express.static(__dirname + '/Scripts'));
+app.use('/Views',  express.static(__dirname + '/Views'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/Views/index.html');
 });
