b0fc6be86b84cb0d05dde8b41206335b059f3647
HibaD <HibaD@users.noreply.github.com>
2018-12-05 00:10:13 -0500

    updated model for express routes

--- models/user.js
+++ models/user.js
@@ -1,17 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const { Property } = require('property')
-const { Claim } = require('claim')
+const { Property } = require('./property.js')
+//const { Claim } = require('claim')
 
-const UserSchema = mongoose.model('User', {
+//creating user Schema
+const userSchema = new mongoose.Schema({
 	fullName: String,
-  username: String,
-  password: String,
-  role: String,
-  email: String,
-  phoneNumber: String,
-  claims: [Claim],
-  property: [Property]
+	username: String,
+	password: String,
+	role: String,
+	email: String,
+	claims: [],
+	property: []
+	//claims: [Claim.schema],
+	//property: [mongoose.model('Property').schema]
 })
 
-module.exports = { UserSchema }
+// add validation of users here
+
+//check if user already exists with pre rule
+
+//create the model to use the schema
+const User = mongoose.model ('User', userSchema)
+
+//make available to users in Node app
+module.exports = { User };
