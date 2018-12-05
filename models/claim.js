592b3c1480a91b95df28e0665061b67eccd38d40
seunghyun kim <dev.sonnyk@gmail.com>
2018-12-05 15:32:54 -0500

    modified models

--- models/claim.js
+++ models/claim.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose')
 
-const { Comments } = require('comment')
+const { Comments } = require('./comment.js')
 
 const ClaimSchema = mongoose.model('Claim', {
   property: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Property'
   },
-	title: String,
-	detail: String,
-  status: String,
+	title: {
+		type: String,
+		required: true
+	},
+	detail: {
+		type: String
+		required: true
+	},
+  status: {
+		type: String
+		required: true
+	},
   comments: [Comments]
 })
 
