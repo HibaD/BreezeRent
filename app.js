e4e307ada6e911d00ff572ff5c8591e3dcbe75a6
HibaD <HibaD@users.noreply.github.com>
2018-12-05 20:44:03 -0500

    added login and admin routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const log = console.log;
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
+const session = require('express-session')
 
 // Import our mongoose connection
 const { mongoose } = require('./db/mongoose');
@@ -20,14 +21,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }))
 
 // static js directory
-app.use(express.static(__dirname));
 app.use('/Styles',  express.static(__dirname + '/Styles'));
 app.use('/Scripts', express.static(__dirname + '/Scripts'));
 app.use('/Views',  express.static(__dirname + '/Views'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Views/index.html');
+// Add express sesssion middleware
+app.use(session({
+	secret: 'oursecret',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		expires: 600000,
+		httpOnly: true
+	}
+}))
+
+// Add middleware to check for logged-in users
+const sessionChecker = (req, res, next) => {
+	if (req.session.user) {
+		res.redirect('dashboard')
+	} else {
+		next();
+	}
+}
+
+// route for root; redirect to login
+app.get('/', sessionChecker, (req, res) => {
+	res.redirect('login');
 });
+
+// route for login
+app.route('/login').get(sessionChecker, (req, res) => {
+	res.sendFile(__dirname + '/Views/index.html');
+})
 
 // POST route to add new users
 app.post('/newUser', (req,res) => {
@@ -60,8 +86,11 @@ app.post('/newUser', (req,res) => {
 	})
 })
 
-app.post('/users/login', (req, res) => {
-	User.findOne({username: req.body.username, password: req.body.password}).then((result)=>{
+app.get('/dashboard', (req, res) => {
+	// check if we have active session cookie
+	if (req.session.user != null) {
+		User.findById(req.session.user).then((result)=>{
+		
 		if (result.role == 'landlord'){
 			res.sendFile(__dirname + '/Views/main.html');
 		}else if (result.role == 'admin'){
@@ -72,6 +101,58 @@ app.post('/users/login', (req, res) => {
 		
 	}).catch ((error) => {
 		res.status(400).send(error) // 400 for bad request
+	})
+	} else {
+		res.redirect('/')
+	}
+})
+
+app.post('/findUser', (req, res) => {
+	User.findOne({username: req.body.username}).then((result)=>{
+		res.send(result)
+		console.log(req.session.user);
+	}).catch((error) => {
+		res.status(400).send(error)
+	})
+})
+
+app.post('/user', (req, res) => {
+	User.findOne({username: req.body.username}).then((result)=>{
+		result.username = req.body.username;
+		result.email = req.body.email;
+		result.fullName = req.body.fullName;
+		result.save();
+		res.send(result)
+	}).catch((error) => {
+		res.status(400).send(error)
+	})
+})
+
+app.get('/users', (req, res) => {
+	User.find({}).then((result)=>{
+		res.send({result});
+	}).catch((error) => {
+		res.status(400).send(error)
+	})
+})
+
+app.post('/users/login', (req, res) => {
+	User.findOne({username: req.body.username, password: req.body.password}).then((result)=>{
+		req.session.user = result._id;
+		req.session.email = result.email
+		res.redirect('/dashboard');
+	}).catch ((error) => {
+		res.status(400).redirect('/login') // 400 for bad request
+	})
+})
+
+app.get('/users/logout', (req, res) => {
+	req.session.destroy((error) => {
+		if (error) {
+			res.status(500).send('error')
+		} else {
+			res.redirect('/')
+		}
 	})
 })
 
