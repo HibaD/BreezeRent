1a57523be08fdc6ef9c950d5e8464e12617e4f76
seunghyun kim <dev.sonnyk@gmail.com>
2018-12-06 19:53:36 -0500

    inserted post request for adding tenant

--- Scripts/propertysummary.js
+++ Scripts/propertysummary.js
@@ -11,11 +11,11 @@ var count_notice = 1;
 
 var today = new Date();
 var dd = today.getDate();
-var mm = today.getMonth()+1; 
+var mm = today.getMonth()+1;
 
 if(dd<10) {
     dd = '0'+dd
-} 
+}
 
 if(mm<10) {
     mm = '0'+mm
@@ -27,28 +27,52 @@ function updateTenantsInfo(e) {
     e.preventDefault();
     count_tenant ++;
     const userTable = document.querySelector('#user-table');
-    
+
     //DOM Manipulation
     const tbodyElement = document.createElement('tr');
     const thElement = document.createElement('th');
     thElement.setAttribute("scope", "row");
     thElement.appendChild(document.createTextNode(count_tenant));
-    const tdfirst = document.createElement('td')
-    tdfirst.appendChild(document.createTextNode("Camelo"))
 
-    const tdsecond = document.createElement('td')
-    tdsecond.appendChild(document.createTextNode("Anthony"))
+    // const tdfirst = document.createElement('td')
+    // tdfirst.appendChild(document.createTextNode("Camelo"))
+    //
+    // const tdsecond = document.createElement('td')
+    // tdsecond.appendChild(document.createTextNode("Anthony"))
+    //
+    // const tdthird = document.createElement('td')
+    // tdthird.appendChild(document.createTextNode("Melo"))
 
-    const tdthird = document.createElement('td')
-    tdthird.appendChild(document.createTextNode("Melo"))
+    const url = '/property';
+    const request = new Request (url, {
+        method: 'post',
+        body: JSON.stringify(ids),
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+    });
+    fetch(request)
+      .then((res) => {
+          if (res.status === 200) {
+             return res.json()
+         } else {
+              alert('Could not get users')
+         }
+      })
+      .then((json) => {
+        tbodyElement.appendChild(thElement);
+        const tdfirst = document.createElement('td');
+        tdfirst.appendChild(document.createTextNode(""));
+        tbodyElement.appendChild(tdfirst);
+        userTable.appendChild(tbodyElement);
 
-    tbodyElement.appendChild(thElement);
-    tbodyElement.appendChild(tdfirst)
-    tbodyElement.appendChild(tdsecond)
-    tbodyElement.appendChild(tdthird)
+      }).catch((error) => {
+          console.log(error)
+      })
 
-    userTable.appendChild(tbodyElement);
-    
+
+
 }
 
 function updateNoticeInfo(e) {
@@ -57,7 +81,7 @@ function updateNoticeInfo(e) {
     const noticetext = document.querySelector('#notice-input').value;
     const noticeTable = document.querySelector('#notice-table');
 
-  
+
     //DOM Manipulation
     const tbodyElement = document.createElement('tr');
     const thElement = document.createElement('th');
@@ -75,7 +99,5 @@ function updateNoticeInfo(e) {
     tbodyElement.appendChild(tdsecond)
 
     noticeTable.appendChild(tbodyElement);
-    
+
 }
-
-
